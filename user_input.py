9d56b5e6b9003ec432a6fbdaffcf097656f635a7
Mbelenga <mbelengaterence@gmail.com>
2024-02-27 01:57:42 -0800

    to be updated

---
+++ user_input.py
@@ -0,0 +1,13 @@
+# Asking user for their name
+
+name = input{"What is your name?"}
+
+# Asking user for their age
+
+age = input{"How old are you?"}
+
+# Asking for their location
+
+location = input{"Where do you live?"}
+
+# Printing out the messagegit add .
