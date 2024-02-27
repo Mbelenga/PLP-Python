ec2ac9ae29222c784cedc6cd42037c1dc1f8e021
Mbelenga <mbelengaterence@gmail.com>
2024-02-27 10:34:45 +0000

    user input

--- user_input.py
+++ user_input.py
@@ -1,15 +1,12 @@
 # Asking user for their name
-
-name = input("What is your name?")
+ name = input("What is your name? ")
 
 # Asking user for their age
-
-age = input("How old are you?")
+age = input("How old are you? ")
 
 # Asking for their location
-
-location = input("Where do you live?")
+location = input("Where do you live? ")
 
 # Printing out the output
-
 print(f"Hello {name}, you are {age} years old and live in {location}.")
+
